b679e49a7122fb84f85c8afbec3417eff3d8b47a
Айгерим Абдурахманова < >
2016-05-13 23:50:05 +0600

    end

---
+++ end2/end2/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace end2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.move_object = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // move_object
+            // 
+            this.move_object.Tick += new System.EventHandler(this.move_object_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 483);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer move_object;
+    }
+}
+
---
+++ end2/end2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace end2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p;
+        int x = 0, y = 0, w = 30, h = 30;
+        int dx = 10, dy = 10;
+        Timer t;
+        SolidBrush b;
+        private void move_object_Tick(object sender, EventArgs e)
+        {
+            if (x + w > Width)
+            {
+                dx = -10;
+                b = new SolidBrush(Color.Red);
+            }
+            else if (x < 0)
+            {
+                dx = 10;
+                b = new SolidBrush(Color.Blue);
+            }
+            if (y + h > Height)
+            {
+                dy = -10;
+                b = new SolidBrush(Color.Yellow);
+            }
+            else if (y < 0)
+            {
+                dy = 10;
+                b = new SolidBrush(Color.Green);
+            }
+            x += dx;
+            y += dy;
+            Refresh();
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //g.DrawEllipse(p,x,y,w,h); 
+            g.FillEllipse(b,x,y,w,h);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+            p = new Pen(Color.Black);
+            b = new SolidBrush(Color.Red);
+            t = new Timer();
+            t.Tick += new EventHandler(move_object_Tick);
+            t.Start();
+        }
+    }
+}
